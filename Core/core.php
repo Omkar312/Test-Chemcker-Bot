d96b19e98ce66f15f4c183d151ba7ea2a5189361
Omkar312 <114221677+Omkar312@users.noreply.github.com>
2025-06-09 15:53:46 +0530

    Update core.php

--- Core/core.php
+++ Core/core.php
@@ -1,6 +1,6 @@
 <?php
 
-$botToken =  "5980177112:AAF2saZaYIej03yT5_UE3Gbjlpgi1g7tnyw";
+$botToken =  "7238698244:AAEihx18tdEjumbCsnPHL0IYYVQ5D3psVWc";
 $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
